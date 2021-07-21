f9c77b6bcc3af9998758cb43172c19dfde9c6931
Aadil Sadik Mohmed Shaikh <ad979991@dal.ca>
2021-07-21 10:23:23 +0530

    Setup the basic webpage

---
+++ halifax-foodie/src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Halifax Foodie</h1>
+        <p className="text-muted">A simple food delivery app!</p>
+      </div>
+    </div>
+  );
+}
