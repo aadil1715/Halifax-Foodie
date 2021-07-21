9f7d99c5e3730819f667f9118020796d9e5f16f3
Aadil Sadik Mohmed Shaikh <ad979991@dal.ca>
2021-07-21 11:43:19 +0530

    Login page added

--- halifax-foodie/src/Routes.js
+++ halifax-foodie/src/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
+import Login from "./containers/Login";
 
 export default function Routes() {
   return (
@@ -9,9 +10,13 @@ export default function Routes() {
       <Route exact path="/">
         <Home />
       </Route>
+      <Route exact path="/login">
+        <Login />
+      </Route>
       <Route>
         <NotFound />
       </Route>
+      
     </Switch>
   );
 }
--- halifax-foodie/src/config.js
+++ halifax-foodie/src/config.js
@@ -1,9 +1,8 @@
 const config = {
     cognito: {
         REGION: "us-east-1",
-        USER_POOL_ID: "arn:aws:cognito-idp:us-east-1:201627978596:userpool/us-east-1_pWng3oGqi",
+        USER_POOL_ID: "us-east-1_pWng3oGqi",
         APP_CLIENT_ID: "1blrgi0dcjbp0pbakfhq0nup4n",
-        IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID",
       },
 };
 
