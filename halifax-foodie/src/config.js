17271448b03a5c13d86ef6907bbb40504f6ef5ac
Aadil Sadik Mohmed Shaikh <ad979991@dal.ca>
2021-07-21 10:47:39 +0530

    Setup the config file

---
+++ halifax-foodie/src/config.js
@@ -0,0 +1,10 @@
+const config = {
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "arn:aws:cognito-idp:us-east-1:201627978596:userpool/us-east-1_pWng3oGqi",
+        APP_CLIENT_ID: "1blrgi0dcjbp0pbakfhq0nup4n",
+        IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID",
+      },
+};
+
+export default config;
